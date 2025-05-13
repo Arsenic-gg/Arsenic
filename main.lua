c2c9e433a45945bafb864cfc783c0791b82e28ba
Arsenic-gg <arsenicbests@gmail.com>
2025-05-13 16:32:41 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+repeat task.wait() until game:IsLoaded() task.wait()
+
+if game.PlaceId == 11729688377 or game.PlaceId == 11879754496 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2160f00537bac611bb821512dfb6b28d.lua"))()
+end
