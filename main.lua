1e47e97af7b1cf16c938b27c48207eda0bbbda08
Arsenic-gg <arsenicbests@gmail.com>
2025-12-12 21:17:15 +0200

    Update notification title and clipboard link

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 11729688377 or game.PlaceId == 11879754496 then
     game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "New Discord Server",
+        Title = "Arsenic Discontinued. New Server & Project [ LUMINA ]",
         Text = "Copied to clipboard",
-        Duration = 5
+        Duration = 10
     })
-    setclipboard("https://discord.gg/CHRKSkrTCu")
+    setclipboard("https://discord.gg/sMX3D24j")
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\97\112\105\46\108\117\97\114\109\111\114\46\110\101\116\47\102\105\108\101\115\47\118\51\47\108\111\97\100\101\114\115\47\51\98\100\53\97\56\55\54\49\97\51\100\50\101\53\49\51\98\50\98\56\99\48\53\52\52\54\51\48\101\50\102\46\108\117\97\34\41\41\40\41\10")()
 end
